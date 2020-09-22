a23e4c0b14a9323029e2e958f54ea7a766224c8b
rachel613g <rachelgutmann613@gmail.com>
2020-09-22 17:32:09 -0400

    removed extraneous comments with unimplemented code

--- src/main/java/touro/snake/SnakeMouseListener.java
+++ src/main/java/touro/snake/SnakeMouseListener.java
@@ -22,35 +22,4 @@ public class SnakeMouseListener extends MouseMotionAdapter {
             snake.turnTo(Direction.West);
         }
     }
-
-    /*
-    Below I began implementing what would be if there were NorthWest,
-    North, and NorthEast directions. This is not finished.
-     */
-
-//    @Override
-//    public void mouseMoved(MouseEvent e) {
-//        //mouse is either NW, N or NE of snake head
-//        if(e.getY() > snake.getHead().getY())
-//        {
-//            //mouse is NW of snake head
-//            if(isMajorDiagonal(e.getX(), e.getY(),
-//                    snake.getHead().getX(), snake.getHead().getY()))
-//            {
-//
-//            }
-//
-//            //mouse is N of snake head
-//        }
-//    }
-//
-//    private boolean isMajorDiagonal(int mouseX, int mouseY, int snakeX, int snakeY)
-//    {
-//        throw new UnsupportedOperationException("not implemented yet");
-//    }
-//
-//    private boolean isMinorDiagonal(int mouseX, int mouseY, int snakeX, int snakeY)
-//    {
-//        throw new UnsupportedOperationException("not implemented yet");
-//    }
 }
