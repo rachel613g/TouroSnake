d612a0f75f48b9f5ecccc2a12f3cb85442989de0
rachel613g <rachelgutmann613@gmail.com>
2020-11-05 04:52:55 -0500

    fixed bug in algorithm. implemented setShortestPath() and getSearchSpace() in gutmann.AStarStrategy

--- src/main/java/touro/snake/strategy/astar/gutmann/AStarStrategy.java
+++ src/main/java/touro/snake/strategy/astar/gutmann/AStarStrategy.java
@@ -8,20 +8,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AStarStrategy implements SnakeStrategy {
-    private Snake snake;
     private List<Node> closed;
     private List<Node> open;
-    private List<Node> shortestPath;
+    private Square head;
+    private Food food;
+    private List<Square> shortestPath;
+    private Direction directions[] = Direction.values();
 
     @Override
     public void turnSnake(Snake snake, Garden garden) {
-        this.snake = snake;
 
-        Square head = snake.getHead();
-        Food food = garden.getFood();
+        this.head = snake.getHead();
+        this.food = garden.getFood();
 
-        if (food == null)
+        if (food == null) {
             return;
+        }
 
         closed = new ArrayList<>();
         open = new ArrayList<>();
@@ -30,12 +32,14 @@ public class AStarStrategy implements SnakeStrategy {
         open.add(new Node(head));
 
 
-        while (open.size() > 0) {
+        while (!open.isEmpty()) {
             Node current = getLowestFCost();
+
             //if reached target
             if (current.equals(food)) {
                 Node firstChild = getFirstChild(head, current);
                 Direction direction = head.directionTo(firstChild);
+                setShortestPath(current);
                 snake.turnTo(direction);
                 break;
             }
@@ -47,7 +51,7 @@ public class AStarStrategy implements SnakeStrategy {
             for (Direction d : Direction.values()) {
                 Node neighbor = new Node(current.moveTo(d), current, food);
                 //neighbor is not traversable or have already visited
-                if (neighbor.inBounds() || closed.contains(neighbor) || snake.contains(neighbor)) {
+                if (!neighbor.inBounds() || closed.contains(neighbor) || snake.contains(neighbor)) {
                     continue;
                 }
                 //determine if there is a shorter path from start node to neighbor
@@ -59,24 +63,29 @@ public class AStarStrategy implements SnakeStrategy {
                         open.remove(index);
                         open.add(neighbor);
                     }
-                } else open.add(neighbor);
-
+                } else {
+                    open.add(neighbor);
+                }
             }
         }
     }
 
     @Override
     public List<Square> getPath() {
-        return null;
+        return shortestPath;
     }
 
     @Override
     public List<Square> getSearchSpace() {
-        return null;
+        return List.copyOf(closed);
     }
 
     private void setShortestPath(Node current) {
-
+        shortestPath = new ArrayList<>();
+        while (!current.getParent().equals(head)) {
+            shortestPath.add(current.getParent());
+            current = current.getParent();
+        }
     }
 
     private Node getLowestFCost() {
